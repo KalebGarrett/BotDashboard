ce3d2757f4178247e50a84ea4390904dc60ed5d3
Kaleb Garrett <kalebagarrett@gmail.com>
2024-05-07 16:59:05 -0400

    Added docker images

---
+++ BotDashboard.App/Constants/DockerImages.cs
@@ -0,0 +1,10 @@
+﻿namespace BotDashboard.App.Constants;
+
+public class DockerImages
+{
+    public const string JokeBot = "kalebg08/jokebot";
+    public const string TriviaBot = "kalebg08/triviabot";
+    public const string FactBot = "kalebg08/factbot";
+    public const string PremBot = "kalebg08/prembot";
+    public const string JamJunction = "kalebg08/jamjunction";
+}
