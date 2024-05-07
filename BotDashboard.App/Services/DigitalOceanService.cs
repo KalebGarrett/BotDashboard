2141647e141f16d3c6f35894a6af66baed74496c
Kaleb Garrett <kalebagarrett@gmail.com>
2024-05-07 18:00:51 -0400

    Added service for Digital Ocean

---
+++ BotDashboard.App/Services/DigitalOceanService.cs
@@ -0,0 +1,23 @@
+﻿using BotDashboard.App.Commands;
+using Renci.SshNet;
+
+namespace BotDashboard.App.Services;
+
+public class DigitalOceanService
+{
+    public readonly SshClient _sshClient;
+    public readonly DockerCommand _dockerCommand;
+
+    public DigitalOceanService(SshClient sshClient, DockerCommand dockerCommand)
+    {
+        _sshClient = sshClient;
+        _dockerCommand = dockerCommand;
+    }
+
+    public void RunImage(string imageName)
+    {
+        _sshClient.Connect();
+        _sshClient.RunCommand($"{_dockerCommand.Run(imageName)}");
+        _sshClient.Disconnect();
+    }
+}
