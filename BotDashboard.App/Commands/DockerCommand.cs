096d6a0add0377dbd3c9eeecb16c5c8aee627f30
Kaleb Garrett <kalebagarrett@gmail.com>
2024-05-07 16:58:47 -0400

    Added docker commands

---
+++ BotDashboard.App/Commands/DockerCommand.cs
@@ -0,0 +1,34 @@
+﻿namespace BotDashboard.App.Commands;
+
+public class DockerCommand
+{
+    public string Run(string imageName)
+    {
+        return $"docker run {imageName}";
+    }
+
+    public string Stop(string containerId)
+    {
+        return $"docker stop {containerId}";
+    }
+
+    public string StopAll()
+    {
+        return "docker stop $(docker ps -q)";
+    }
+
+    public string RunningContainers()
+    {
+        return "docker ps";
+    }
+
+    public string Stats()
+    {
+        return "docker ls";
+    }
+
+    public string Pull(string imageName)
+    {
+        return $"docker pull {imageName}";
+    }
+}
