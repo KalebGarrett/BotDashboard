47df72e750efb5c55331bdb8e45f09ce2cc79caf
Kaleb Garrett <kalebagarrett@gmail.com>
2024-05-28 17:11:48 -0400

    Add MudBlazor button groups for commands

--- BotDashboard.App/Components/Pages/Commands.cs
+++ BotDashboard.App/Components/Pages/Commands.cs
@@ -8,13 +8,25 @@ public partial class Commands
 {
     [Inject] public DockerService DockerService { get; set; }
     private List<DockerImage> DockerImages { get; set; } = new();
-    private string[] Headings { get; set; } = {"Repository", "Tag", "Image Id", "Created", "Size"};
+    private string[] Headings { get; set; } = {"Repository", "Tag", "Image Id", "Created", "Size", "Actions"};
 
     protected override async Task OnInitializedAsync()
     {
         ListImages();
     }
 
+    private void RunImage(string imageName)
+    {
+        DockerService.RunImage(imageName);
+    }
+
+    private void PullImage(string imageName)
+    {
+        DockerService.PullImage(imageName);
+        Task.Delay(TimeSpan.FromSeconds(10));
+        ListImages();
+    }
+    
     private void ListImages()
     {
         DockerImages = DockerService.ListImages();
