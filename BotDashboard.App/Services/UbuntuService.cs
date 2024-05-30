f126f1d84ac0b2b703e2f413611591835f8598eb
Kaleb Garrett <kalebagarrett@gmail.com>
2024-05-30 17:27:08 -0400

    Added service for Ubuntu commands

---
+++ BotDashboard.App/Services/UbuntuService.cs
@@ -0,0 +1,32 @@
+﻿using BotDashboard.App.Commands;
+using BotDashboard.App.Events;
+using BotDashboard.App.Secrets;
+using BotDashboard.Models;
+using Renci.SshNet;
+
+namespace BotDashboard.App.Services;
+
+public class UbuntuService
+{
+    public readonly UbuntuCommand _UbuntuCommand;
+
+    public UbuntuService(UbuntuCommand ubuntuCommand)
+    {
+        _UbuntuCommand = ubuntuCommand;
+    }
+    
+    public string MemoryUsagePercentage()
+    {
+        using var client = new SshClient(DigitalOcean.Host, DigitalOcean.Username, DigitalOcean.Password);
+        client.HostKeyReceived += SshEvents.ClientOnHostKeyReceived;
+        client.Connect();
+
+        var command = client.CreateCommand(_UbuntuCommand.ListMemoryStats());
+        var response = command.Execute();
+
+        client.HostKeyReceived -= SshEvents.ClientOnHostKeyReceived;
+        client.Disconnect();
+
+        return response.Remove(0, 83).Substring(0,2);
+    }
+}
