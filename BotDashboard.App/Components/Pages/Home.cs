2889ef0a363056c393119a29b25871a67ed6ce0d
Kaleb Garrett <kalebagarrett@gmail.com>
2026-01-21 19:41:14 -0500

    Align command and stat card heights; add lock feature to buttons

--- BotDashboard.App/Components/Pages/Home.cs
+++ BotDashboard.App/Components/Pages/Home.cs
@@ -10,95 +10,153 @@ public partial class Home
 {
     [Inject] private DockerService DockerService { get; set; }
     [Inject] private UbuntuService UbuntuService { get; set; }
-   
+
     private List<Container> Containers { get; set; } = new();
     private string ContainerFetchTime { get; set; }
-    
+
     public double[] MemoryUsageData { get; set; }
     public string[] MemoryUsageLabels { get; set; } = ["Used Memory", "Total Memory"];
     private static double MemoryUsagePercentage { get; set; }
-    
+
     private double[] CpuUsageData { get; set; }
     private string[] CpuUsageLabels { get; set; } = ["Used CPU", "Total CPU"];
     private static double CpuUsagePercentage { get; set; }
-    
+
     private string StatFetchTime { get; set; }
+
+    private bool Locked { get; set; }
 
     protected override async Task OnInitializedAsync()
     {
-      if (Switch.OnInitialized == false)
-      {
-          MemoryUsagePercentage = 0;
-          await Task.CompletedTask;
-          return;
-      }
-      
-      ListContainers(showSnackbar: false);
-      ListStats(showSnackbar: false);
+        if (Switch.OnInitialized == false)
+        {
+            MemoryUsagePercentage = 0;
+            CpuUsagePercentage = 0;
+            await Task.CompletedTask;
+            return;
+        }
+
+        await ListContainers(isInternalCall: true);
+        await ListStats(isInternalCall: true);
     }
-    
-    private void RunImage(string imageName)
+
+    private async Task RunImage(string imageName)
     {
+        Locked = true;
+        await InvokeAsync(StateHasChanged);
+        await Task.Yield();
+
         DockerService.RunImage(imageName);
-        ListContainers(showSnackbar: false);
+        await ListContainers(isInternalCall: true);
         CreateSnackbarMessage("Successfully ran image!", Severity.Success);
+
+        Locked = false;
+        await InvokeAsync(StateHasChanged);
     }
 
-    private void StopContainer(string containerId)
+    private async Task StopContainer(string containerId)
     {
+        Locked = true;
+        await InvokeAsync(StateHasChanged);
+        await Task.Yield();
+
         DockerService.StopContainer(containerId);
-        ListContainers(showSnackbar: false);
+        await ListContainers(isInternalCall: false);
         CreateSnackbarMessage("Successfully stopped container!", Severity.Success);
+
+        Locked = false;
+        await InvokeAsync(StateHasChanged);
     }
 
-    private void StopAllContainers()
+    private async Task StopAllContainers()
     {
+        Locked = true;
+        await InvokeAsync(StateHasChanged);
+        await Task.Yield();
+
         DockerService.StopAllContainers();
-        ListContainers(showSnackbar: false);
+        await ListContainers(isInternalCall: false);
         CreateSnackbarMessage("Successfully stopped all containers!", Severity.Success);
+
+        Locked = false;
+        await InvokeAsync(StateHasChanged);
     }
 
-    private void RestartContainer(string containerId)
+    private async Task RestartContainer(string containerId)
     {
+        Locked = true;
+        await InvokeAsync(StateHasChanged);
+        await Task.Yield();
+
         DockerService.RestartContainer(containerId);
-        ListContainers(showSnackbar: false);
+        await ListContainers(isInternalCall: false);
         CreateSnackbarMessage("Successfully restarted container!", Severity.Success);
+
+        Locked = false;
+        await InvokeAsync(StateHasChanged);
     }
 
-    private void RestartAllContainers()
+    private async Task RestartAllContainers()
     {
+        Locked = true;
+        await InvokeAsync(StateHasChanged);
+        await Task.Yield();
+
         DockerService.RestartAllContainers();
-        ListContainers(showSnackbar: false);
+        await ListContainers(isInternalCall: false);
         CreateSnackbarMessage("Successfully restarted all containers!", Severity.Success);
+
+        Locked = false;
+        await InvokeAsync(StateHasChanged);
     }
 
-    private void ListContainers(bool showSnackbar = true)
+    private async Task ListContainers(bool isInternalCall = false)
     {
+        if (!isInternalCall)
+        {
+            Locked = true;
+            await InvokeAsync(StateHasChanged);
+            await Task.Yield();
+        }
+
         Containers = DockerService.ListContainers();
         ContainerFetchTime = DateTime.UtcNow.ToLocalTime().ToString(CultureInfo.CurrentCulture);
 
-        if (showSnackbar)
+        if (!isInternalCall)
         {
             CreateSnackbarMessage("Successfully listed containers!", Severity.Success);
         }
+
+        Locked = false;
+        await InvokeAsync(StateHasChanged);
     }
-    
-    private void ListStats(bool showSnackbar = true)
+
+    private async Task ListStats(bool isInternalCall = false)
     {
+        if (!isInternalCall)
+        {
+            Locked = true;
+            await InvokeAsync(StateHasChanged);
+            await Task.Yield();
+        }
+
         MemoryUsagePercentage = UbuntuService.MemoryUsagePercentage();
         MemoryUsageData = [MemoryUsagePercentage, 100 - MemoryUsagePercentage];
-        
+
         CpuUsagePercentage = UbuntuService.CpuUsagePercentage();
         CpuUsageData = [CpuUsagePercentage, 100 - CpuUsagePercentage];
-        
+
         StatFetchTime = DateTime.UtcNow.ToLocalTime().ToString(CultureInfo.CurrentCulture);
-        
-        if (showSnackbar)
+
+        if (!isInternalCall)
         {
             CreateSnackbarMessage("Successfully listed stats!", Severity.Success);
         }
+
+        Locked = false;
+        await InvokeAsync(StateHasChanged);
     }
-    
+
     private void CreateSnackbarMessage(string message, Severity severity)
     {
         Snackbar.Configuration.PositionClass = Defaults.Classes.Position.BottomRight;
