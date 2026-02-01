39b1e3adbe72f874d2f587e91c07b286f203e6f0
Kaleb Garrett <kalebagarrett@gmail.com>
2026-02-01 18:10:51 -0500

    Added user profile model

---
+++ BotDashboard.Models/Authentication/UserProfile.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BotDashboard.Models.Authentication;
+
+public class UserProfile
+{
+    [Required] public string Id { get; set; }
+    [Required] public string Name { get; set; }
+    [Required] public string Email { get; set; }
+    [Required] public string PhoneNumber { get; set; }
+}
