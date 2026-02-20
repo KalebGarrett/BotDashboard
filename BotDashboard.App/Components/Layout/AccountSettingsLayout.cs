e089c7ff91b7eedb2df8bff0eafbe44ff8957525
Kaleb Garrett <kalebagarrett@gmail.com>
2026-02-20 13:21:50 -0500

    Added account settings layout

---
+++ BotDashboard.App/Components/Layout/AccountSettingsLayout.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using MudBlazor;
+
+namespace BotDashboard.App.Components.Layout;
+
+public partial class AccountSettingsLayout
+{
+    private bool Open { get; set; } = true;
+    private ErrorBoundary ErrorBoundary { get; set; } = new();
+    
+    [Inject] private NavigationManager NavigationManager { get; set; }
+
+    private RenderFragment GetErrorContent()
+    {
+        return _ =>
+        {
+            Snackbar.Configuration.PositionClass = Defaults.Classes.Position.BottomRight;
+            Snackbar.Configuration.PositionClass = Defaults.Classes.Position.BottomRight;
+            Snackbar.Add("Oops… something went wrong. Please try again.", Severity.Error);
+        };
+    }
+    
+    protected override void OnParametersSet()
+    {
+        ErrorBoundary.Recover();
+    }
+    
+    private void ToggleDrawer()
+    {
+        Open = !Open;
+    }
+
+    private void Logout()
+    {
+        NavigationManager.NavigateTo("/logout", true);
+    }
+    
+    private void NavigateToLogin()
+    {
+        NavigationManager.NavigateTo("/login");
+    }
+}
